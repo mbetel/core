96d22706c8098c91708c96d35faf33c1144a1c3e
michielbetel <mbetel@zaak-op-orde.nl>
2025-01-22 13:31:26 +0100

    expiresAt

--- sqlxstore/sqlxstore.go
+++ sqlxstore/sqlxstore.go
@@ -538,10 +538,6 @@ func (store *SqlxStore) update(session *sessions.Session) error {
 		if expiresAt.Before(time.Now()) {
 			expiresAt = time.Now().Add(time.Second * time.Duration(session.Options.MaxAge))
 		}
-		//if expiresAt.Sub(time.Now().Add(time.Second*time.Duration(session.Options.MaxAge))) < 0 { // If session is not expired
-		//	// Refresh expiresAt
-		//	expiresAt = time.Now().Add(time.Second * time.Duration(session.Options.MaxAge))
-		//}
 	}
 
 	// Delete this fields since are stored on the table
